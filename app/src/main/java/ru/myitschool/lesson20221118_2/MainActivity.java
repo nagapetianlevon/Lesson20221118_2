a84b199963f27e5aef30af188a27341e8452646c
nagapetianlevon <leva.nagapetian@gmail.com>
2022-11-18 17:12:34 +0700

    Initial commit

---
+++ app/src/main/java/ru/myitschool/lesson20221118_2/MainActivity.java
@@ -0,0 +1,45 @@
+package ru.myitschool.lesson20221118_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import ru.myitschool.lesson20221118_2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*TextView outputtext;
+    EditText inputName, inputPhone;
+    FloatingActionButton button;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding.getRoot();
+
+
+      //  setContentView(R.layout.activity_main);
+       /* outputtext = (TextView) findViewById(R.id.outputText);
+        inputName = findViewById(R.id.InputName);
+        inputPhone = findViewById(R.id.InputPhone);
+        button = findViewById(R.id.add);*/
+
+
+
+       /* button.setOnClickListener(view -> outputtext.append(inputName.getText().toString()+"\n"+inputPhone.getText().toString())) ;*/
+
+
+
+
+    }
+
+
+}
